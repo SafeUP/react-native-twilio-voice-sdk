5c152810659b1345ec787ad1cd4d8bf8e296b798
nustony <tony@nustechnology.com>
2019-11-19 14:32:27 +0700

    Update dist

--- dist/callError.js
+++ dist/callError.js
@@ -2,7 +2,7 @@ class CallError extends Error {
     constructor(message, reason, code) {
         super(message);
         // See: https://stackoverflow.com/questions/41102060/typescript-extending-error-class
-        Object.setPrototypeOf(this, new.target.prototype);
+        Object.setPrototypeOf(this, CallError.prototype);
         this.code = code;
         this.reason = reason;
     }
